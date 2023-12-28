3ae067fd33550b7f57480e04e76a3e2ac9ec6283
Mohit Wankhade <1998mohitwankhade@gmail.com>
2023-12-28 17:00:54 +0530

    Changes in post-list

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -8,10 +8,5 @@ import { PostCreateComponent } from "../post-create/post-create.component";
 })
 export class PostListComponent  
 {
-    posts = 
-    [
-        {title:'First Post', content:'This is the first post\'s content'},
-        {title:'Second Post', content:'This is the second post\'s content'},
-        {title:'Third Post', content:'This is the third post\'s content'}
-    ];
+    posts = [];
 }
